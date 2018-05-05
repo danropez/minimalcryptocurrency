599a119496aef23a899dc0a51cdebaf183c97fa9
Daniel Rodríguez Pérez <daniel.rodriguez.perez@gmail.com>
2018-05-05 16:39:23 +0200

    Changes for version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="minimalcryptocurrency",
-    version="0.1.0",
+    version="0.1.1",
     url="https://github.com/drodriguezperez/minimalcryptocurrency",
 
     author=u"Daniel Rodríguez Pérez",
